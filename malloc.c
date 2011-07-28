a415d66b46ac233002feec18296a76f99f89168b
Mariellen Ivo <mariellenmivo@gmail.com>
2011-07-28 14:19:19 -0300

    comentarios adicionados

--- malloc.c
+++ malloc.c
@@ -1,3 +1,9 @@
+/*
+ Universidade Estadual de Montes Claros - Unimontes
+ GEAED - Grupo de Estudos em Algoritmos e Estruturas de Dados
+ Descrição: Alocação dinâmica em um vetor extático
+*/
+
 #include<conio.h>
 #include<stdio.h>
 #include<string.h>
@@ -5,23 +11,36 @@
 
 int main() {
     int *vet, i, n;
-    printf("Tamanho do vetor: ");
+
+    printf("\n\t Alocacao dinamica de vetores \n\n");
+
+    // Dados de entrada
+    printf(" Tamanho do vetor: ");
     fflush(stdin);
     scanf("%d", &n);
+
+    // Alocando espaco para o vetor
     vet = (int *) malloc(n*sizeof(int));
+
     if (vet == NULL) {
         printf("Memoria insuficiente.");
         return 1;
     }
     else {
+        // Inserindo elementos
         for (i = 0; i < n; i++) {
-            printf("Valor de %d: ", i+1);
+            printf("Elemento %d: ", i+1);
             fflush(stdin);
             scanf("%d", &vet[i]);
         }
+        printf("\n");
+
+        // Imprimindo o vetor obtido
         for (i = 0; i < n; i++) {
             printf("%d ", vet[i]);
         }
+
+        // Testando se o vetor foi desalocado corretamente
         printf("\n%d", &vet);
         free(vet);
         printf("\n%d", &vet);
